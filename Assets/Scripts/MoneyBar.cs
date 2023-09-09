c09054f45f086bed1c78f05a379cf6efe8f1fd3e
Catlerc <mcr1123@yandex.ru>
2023-09-09 17:34:00 +0500

    coins + text mesh pro

---
+++ Assets/Scripts/MoneyBar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyBar : MonoBehaviour
+{
+    public TextMeshProUGUI moneyText;
+    public MoneyHolder moneyHolder;
+
+    private void Start()
+    {
+        moneyHolder.onCoinsChange.AddListener(changeCoins);
+    }
+
+    private void changeCoins(int coins)
+    {
+        moneyText.text = coins.ToString();
+    }
+}
---
+++ Assets/Scripts/MoneyHolder.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MoneyHolder : MonoBehaviour
+{
+    public int coins;
+    public UnityEvent<int> onCoinsChange = new();
+
+
+    bool canSpend(int amount) => coins >= amount;
+
+    bool spend(int amount)
+    {
+        if (coins >= amount)
+        {
+            coins -= amount;
+            onCoinsChange.Invoke(coins);
+            return true;
+        }
+        else
+            return false;
+    }
+
+    void add(int amount)
+    {
+        coins += amount;
+        onCoinsChange.Invoke(coins);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("q")) add(10); // TODO: нужно только для теста. удалить при рабочих противниках
+    }
+}
